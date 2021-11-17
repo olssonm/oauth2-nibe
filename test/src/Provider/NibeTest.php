d2b5b868c67076809338da3cc42a42d6b011c052
Marcus Olsson <olssonm@users.noreply.github.com>
2021-11-17 08:30:23 +0100

    Add tests (#1)
    * Add tests, update composer dependancies
    
    * phpfix
    
    * Fix composer for older PHP-versions
    
    * Edits tests for back compat
    
    * Add Github workflow
    
    * Remove deprecated  no-suggest-flag

---
+++ test/src/Provider/NibeTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Olssonm\Nibe\OAuth2\Client\Tests\Provider;
+
+use GuzzleHttp\ClientInterface;
+use League\OAuth2\Client\Token\AccessToken;
+use Mockery as m;
+use Olssonm\OAuth2\Client\Provider\Exception\ResourceOwnerException;
+use Olssonm\OAuth2\Client\Provider\Nibe;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+class NibeTest extends TestCase
+{
+    /**
+     * @var BaseProvider
+     */
+    protected $provider;
+
+    public function getProvider()
+    {
+        return new Nibe([
+            'clientId' => 'mock_client_id',
+            'clientSecret' => 'mock_secret',
+            'redirectUri' => 'mock_redirect_uri',
+        ]);
+    }
+
+    public function testAuthorizationUrl()
+    {
+        $provider = $this->getProvider();
+
+        $url = $provider->getAuthorizationUrl();
+        $uri = parse_url($url);
+
+        parse_str($uri['query'], $query);
+
+        $this->assertEquals('https', $uri['scheme']);
+        $this->assertEquals('api.nibeuplink.com', $uri['host']);
+        $this->assertEquals('/oauth/authorize', $uri['path']);
+
+        $this->assertArrayHasKey('client_id', $query);
+        $this->assertArrayHasKey('redirect_uri', $query);
+        $this->assertArrayHasKey('state', $query);
+        $this->assertArrayHasKey('scope', $query);
+        $this->assertArrayHasKey('response_type', $query);
+        $this->assertArrayHasKey('approval_prompt', $query);
+
+        $this->assertNotNull($provider->getState());
+    }
+
+    public function testResourceOwnerDetailsUrl()
+    {
+        $provider = $this->getProvider();
+
+        $this->expectException(ResourceOwnerException::class);
+
+        $token = m::mock(AccessToken::class);
+        $provider->getResourceOwnerDetailsUrl($token);
+    }
+
+    public function testGetAccessToken()
+    {
+        $provider = $this->getProvider();
+
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody')->andReturn('{"access_token":"mock_access_token", "token_type":"bearer"}');
+        $response->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $response->shouldReceive('getStatusCode')->andReturn(200);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')->times(1)->andReturn($response);
+        $provider->setHttpClient($client);
+
+        $token = $provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+
+        $this->assertEquals('mock_access_token', $token->getToken());
+        $this->assertNull($token->getExpires());
+        $this->assertNull($token->getRefreshToken());
+        $this->assertNull($token->getResourceOwnerId());
+    }
+}
